450cc904bd4586507b4e0a4bf26ddf7a31337428
Silas Anastacio <silasas@gmail.com>
2024-05-09 13:24:46 -0300

    Construção da API para o Banco1, acessando dados dos clientes.

---
+++ Banco1/API/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using Banco1.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Banco1.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("get-customer-list")]
+        public async Task<IActionResult> List()
+        {
+            var result = await _context.Customer.ToListAsync();
+
+            if(result != null)
+                return Ok(result);
+
+            return BadRequest();
+        }
+    }
+}
